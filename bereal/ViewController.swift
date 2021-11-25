fd9378f7ebacf1ab9f8d5e0444ac46dc5d29282e
charley <charley.fromage@gmail.com>
2021-11-25 16:10:44 +0100

    Initial commit

---
+++ bereal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bereal
+//
+//  Created by Fromage Charley on 25/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
